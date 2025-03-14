f542eddd85cf2fd0e510bcb0003b0e9d4a7541dc
Anto04100 <antosou04@gmail.com>
2025-03-14 10:13:49 +0100

    ajout page web

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello, Docker! Mon serveur tourne !");
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Servir des fichiers statiques (ex: HTML, CSS, JS)
+app.use(express.static('public'));
+
+// Route principale
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/app/index.html');
+});
+
+// Démarrer le serveur
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
